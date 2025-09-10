af569a28c872e0e09f1ac7db19dc44946ee514fa
stefsayse <stefanie.sayse@lexisnexisrisk.com>
2025-09-10 23:19:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+(function(window){
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for (i in names) {
+	var firstLetter = (names[i].charAt(0)).toLowerCase();
+	 if (firstLetter === 'j') {
+		       byeSpeaker.speak(names[i]);
+		     } else {
+	 helloSpeaker.speak(names[i]);
+    }
+  }
+
+})(window);
